464b4dc1aea195dd6690e78251044351370a295f
Fernandovj <44930992+Fernandovj@users.noreply.github.com>
2020-12-21 08:23:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,7 @@ total_episodes = 100
 max_steps = 100
 alpha = 0.5
 gamma = 1
-""" 
-	The two parameters below is used to calculate 
-	the reward by each algorithm 
-"""
+
 episodeReward = 0
 totalReward = {
 	'silly':[], 
@@ -61,49 +58,51 @@ qLearningAgent = QLearningAgent(
 
 #print("action_space:",env.action_space)
 #print("state_space size:",env.n_state_space)
-agent = qLearningAgent
+agents = [qLearningAgent]
 
 start = time.time()
 
-for e in range(total_episodes): 
-	# Initialize the necesary parameters before 
-	# the start of the episode 
-	t = 0
-	state1 = env.reset() 
-	print("first_state:",state1)
-	action1 = agent.choose_action(state1)
-	print("action1",action1)
-	#action1 = random.choice(env.action_space) 
-	episodeReward = 0
-	episodeAPCs = []
-	while t < max_steps: 
+for agent in agents:
+	for e in range(total_episodes): 
+		# Initialize the necesary parameters before 
+		# the start of the episode 
+		t = 0
+		state1 = env.reset() 
+		print("first_state:",state1)
+		action1 = agent.choose_action(state1)
+		print("action1",action1)
+		#action1 = random.choice(env.action_space) 
+		episodeReward = 0
+		episodeAPCs = 0
+		while t < max_steps: 
 
-	# 	# Getting the next state, reward, and other parameters 
-		state2, reward, done, info = env.step(action1)
-		print("Ep",e,"t:",t+1,"next_state:",state2,"reward:",round(reward))
-	# 	# Choosing the next action 
-		action2 = agent.choose_action(state2) 
-		#action2 = random.choice(env.action_space)
-		
-	# 	# Learning the Q-value 
-		agent.update(state1, state2, reward, action1, action2)
-		state1 = state2
-		action1 = action2 
-		
-	# 	# Updating the respective vaLues 
-		t += 1
-		episodeReward += reward 
-		#episodeAPCs.append(info["APC"])
-		
-	# 	# If at the end of learning process 
-		if done:
-			break
+		# 	# Getting the next state, reward, and other parameters 
+			state2, reward, done, info = env.step(action1)
+			print("Ep",e,"t:",t+1,"next_state:",state2,"reward:",round(reward))
+		# 	# Choosing the next action 
+			action2 = agent.choose_action(state2) 
+			#print("action:",action2)
+			#action2 = random.choice(env.action_space)
+			
+		# 	# Learning the Q-value 
+			agent.update(state1, state2, reward, action1, action2)
+			state1 = state2
+			action1 = action2 
+			
+		# 	# Updating the respective vaLues 
+			t += 1
+			episodeReward += reward 
+			#sepisodeAPCs += info["APC"]
+			
+		# 	# If at the end of learning process 
+			if done:
+				break
 
-	# Append the sum of reward at the end of the episode 
-	totalReward[type(agent).__name__].append(episodeReward)
-	#totalReward["silly"].append(round(episodeReward,2)) 
-	#APC["silly"].append(episodeAPCs) 
-#env.close() 
+		# Append the sum of reward at the end of the episode 
+		totalReward[type(agent).__name__].append(episodeReward)
+		#totalReward["silly"].append(round(episodeReward,2)) 
+		#sAPC[type(agent).__name__].append(episodeAPCs) 
+
 
 end = time.time()
 print("total time: " + str(end-start))
@@ -124,5 +123,6 @@ print(totalReward)
 
 
 #plt.plot(totalReward["silly"])
-plt.plot(totalReward[type(agent).__name__])
+plt.plot(totalReward["QLearningAgent"])
+#splt.plot(APC[type(agent).__name__])
 plt.show()
